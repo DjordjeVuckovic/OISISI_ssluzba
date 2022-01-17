e35b8fa6a97e670896971ad4297e4dbd174b18fa
Djordje Vuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-01-17 22:39:00 +0100

    trim searched text

--- src/gui/view/search/SearchStudent.java
+++ src/gui/view/search/SearchStudent.java
@@ -17,12 +17,12 @@ public class SearchStudent {
             BazaStudenata.getInstance().setSearchMode(true);
         }
         ArrayList<Student> search = new ArrayList<>();
-        string = string.toLowerCase();
+        string.trim().toLowerCase();
         String[] arraySt =string.split(",");
         // sw-1-2019, Marko, Marković
         if(arraySt.length ==3){
             for(Student student :BazaStudenata.getInstance().getStudents()){
-                if(student.getIndex().toLowerCase().contains(arraySt[0]) && student.getName().toLowerCase().contains(arraySt[1]) && student.getSurname().contains(arraySt[2])){
+                if(student.getIndex().toLowerCase().contains(arraySt[0].trim()) && student.getName().toLowerCase().contains(arraySt[1].trim()) && student.getSurname().contains(arraySt[2].trim())){
                     search.add(student);
                 }
             }
@@ -30,7 +30,7 @@ public class SearchStudent {
         //surname,name
         else if(arraySt.length ==2){
             for(Student student :BazaStudenata.getInstance().getStudents()){
-                if(student.getSurname().toLowerCase().contains(arraySt[0]) && student.getName().toLowerCase().contains(arraySt[1])){
+                if(student.getSurname().toLowerCase().contains(arraySt[0].trim()) && student.getName().toLowerCase().contains(arraySt[1].trim())){
                     search.add(student);
                 }
             }
@@ -38,7 +38,7 @@ public class SearchStudent {
         //surname
         else if(arraySt.length ==1){
             for(Student student:BazaStudenata.getInstance().getStudents()){
-                if(student.getSurname().toLowerCase().contains(arraySt[0])){
+                if(student.getSurname().toLowerCase().contains(arraySt[0].trim())){
                     search.add(student);
                 }
             }
